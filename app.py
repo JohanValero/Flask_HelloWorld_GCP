32af75aa901e95664b75a4c6333b7a059e2b27e7
unknown <Johan.Pool.1994@gmail.com>
2022-07-18 18:01:01 -0500

    Modified app.py for show time

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola mundo"
+    currentDateAndTime = datetime.now()
+    return "Hola mundo: " + currentDateAndTime.strftime("%H:%M:%S")
 
 app.run(host='0.0.0.0', port=81)
