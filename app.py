10a7fcf3b93def5cff2b18e9508eb06b451b58ba
unknown <Johan.Pool.1994@gmail.com>
2022-08-27 19:15:18 -0500

    Uso de más env vars

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     currentDateAndTime = datetime.now()
-    return "Hola mundo de Johan: " + currentDateAndTime.strftime("%H:%M:%S")
+    vStringData = "Hola mundo de " + str(gAUTHOR_NAME) + " in port [" + str(gPORT) + "]: " + currentDateAndTime.strftime("%H:%M:%S")
+    return vStringData
 
 gPORT = os.getenv('PORT', default=None)
+gAUTHOR_NAME = os.getenv("AUTHOR", default=None)
 print("PORT: ", gPORT)
 
 app.run(host='0.0.0.0', port=gPORT)
